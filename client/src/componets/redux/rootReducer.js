4ff6b1a06bd21b498fd749354838b862b288e0e8
tinku gupta <tinku.gupta4@gmail.com>
2022-09-24 00:35:56 +0530

    add api to homepage

---
+++ client/src/componets/redux/rootReducer.js
@@ -0,0 +1,12 @@
+const intialState = {
+    loading:false,
+    cartItem:[]
+}
+
+export const rootReducer = (state=intialState,action) => {
+
+    switch(action.type) {
+        default:return state;
+    }
+
+};

75104d757db9ceac85588fdd75597004f16d47aa
tinku gupta <tinku.gupta4@gmail.com>
2022-10-11 00:03:51 +0530

    work on cart item page add table 11 oct

--- client/src/componets/pages/CartPage.js
+++ client/src/componets/pages/CartPage.js
@@ -1,10 +1,30 @@
 import React from 'react'
+import DefaultLayout from './../DefaultLayout';
+import { useSelector } from "react-redux"
+import {DeleteOutlined,PlusCircleOutlined,MinusCircleOutlined}  from '@ant-design/icons';
+import { Table } from 'antd';
 
 const CartPage = () => {
+    const {cartItems} = useSelector(state => state.rootReducer)
+
+    const columns = [
+        
+         { title: 'Name', dataIndex: 'name'},
+         { title: 'Image', dataIndex: 'image',render:(image,record) => <img src={image} alt={record.name} height="60" width="60"/>},
+         {title:'Price',dataIndex:'price'},
+         {title:'Quantity', dataIndex:'_id',render:(id,record) => <div>
+            <PlusCircleOutlined className='mx-3'/>
+            <b>{record.quantity}</b>
+            <MinusCircleOutlined className='mx-3'/>
+         </div>},
+         {title:'Action', dataIndex:"_id",render:(id,record)=> <DeleteOutlined/>}
+  
+      ];
+
   return (
-    <div>
-    <h1>cart</h1>  
-    </div>
+    <DefaultLayout>
+   <Table columns={columns} dataSource={cartItems} bordered/>
+    </DefaultLayout>
   )
 }
 
